e26b3c21669e47bddb70231fb8dfb985d3fa63cf
nguyenlak2003 <107288697+nguyenlak2003@users.noreply.github.com>
2024-04-23 10:40:21 +0700

    Revert "Login View done"

--- src/main/java/application/func/UserFunc.java
+++ src/main/java/application/func/UserFunc.java
@@ -10,11 +10,9 @@ import application.entity.User;
 public class UserFunc {
     public boolean checkUser(User user){
         if(user != null) {
-            if ("admin".equals(user.getUserName()) 
-                    && "admin".equals(user.getPassword())) {
-                return true;
-            }
+            
         }
+        
         return false;
     }
 }
