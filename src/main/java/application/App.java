72b4903818bc71ad2482362b29fb7ff9783245cb
nguyenlak2003 <nguyenlak1404@gmail.com>
2024-04-23 14:30:25 +0700

    2:30PM

--- src/main/java/application/App.java
+++ src/main/java/application/App.java
@@ -1,40 +1,13 @@
-
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package application;
-
-import application.controller.LoginController;
-import application.view.LoginView;
-import java.awt.EventQueue;
 
 /**
  *
- * @author Lăk
+ * @author Admin
  */
 public class App {
-    public static void main(String[] args) {
-        EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                setLockAndFeel();
-                new LoginController(new LoginView());
-            }
-
-            private void setLockAndFeel() {
-                try {
-                    for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                        if ("Nimbus".equals(info.getName())) {
-                            javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                            break;
-                        }
-                    }
-                } catch (ClassNotFoundException ex) {
-                    java.util.logging.Logger.getLogger(LoginView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-                } catch (InstantiationException ex) {
-                    java.util.logging.Logger.getLogger(LoginView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-                } catch (IllegalAccessException ex) {
-                    java.util.logging.Logger.getLogger(LoginView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-                } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-                    java.util.logging.Logger.getLogger(LoginView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-                }
-            }
-        });
-    }
+    
 }
